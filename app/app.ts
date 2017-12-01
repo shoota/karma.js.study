53cb006e657ea7efd2aa4a1fb22a8bf2b7e0fb32
Shuta Kumano <shoota.dev@gmail.com>
2017-12-01 12:12:13 +0900

    app to module

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,6 @@
-const add = (a: number, b: number) : number => (
-    a + b
-)
-
-const subtract = (a: number, b: number): number => (
-    a - b
-)
+export function add(a: number, b: number) : number {
+    return a + b
+}
+export function subtract(a: number, b: number): number {
+    return a - b
+}
